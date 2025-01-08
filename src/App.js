36345629f35df7a8289f430b7e04fd9b2e3bc549
cpu <shakemidinovemir@gmail.com>
2025-01-08 12:26:54 +0600

    fist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Title from './components/title/Title'
-import About from './components/about/About'
 import MainPage from "./pages/mainpage/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <About/>
-      <Title/>
       <MainPage/>
     </div>
   );
